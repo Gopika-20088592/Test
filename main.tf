0e9c2ab94d72d7b6759b841413867fd544453040
Gopika-20088592 <20088592@mydbs.ie>
2026-02-14 20:53:10 +0000

    Initial clean commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "my_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-Docker-Server"
+  }
+}
